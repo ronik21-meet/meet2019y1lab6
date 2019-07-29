ff04c4d76cb19891c3aec5d3fa4c68854aea7cbd
ronik21-meet <ronik21@meet.mit.edu>
2019-07-29 05:36:29 -0400

    Stopping here

--- fun1.py
+++ fun1.py
@@ -1,5 +1,19 @@
-total = 0
-for number in range(1, 10 + 1):
-    print(number)
-    total = total + number
-print(total)
+##total = 0
+##for number in range(1, 10 + 1):
+##    print(number)
+##    total = total + number
+##print(total)
+
+
+def add_number():
+    total = 0
+    for number in range(1, 10 + 1):
+        print(number)
+        total = total + number
+        return(total)
+    new_answer=total+10
+
+answer=add_number()
+new_answer=answer+8
+print(new_answer)
+#print(answer)
